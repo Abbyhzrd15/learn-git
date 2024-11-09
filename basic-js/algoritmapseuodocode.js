05f7ffdd505befd162410e6ce44cf2594f27e520
Abbyhzrd15 <abbynu9@gmail.com>
2024-11-09 14:07:01 +0700

    latihan algoritmapseudocode

---
+++ basic-js/algoritmapseuodocode.js
@@ -0,0 +1,64 @@
+/**
+ * Study Case 1
+ * Buat algoritma, pseudocode, dan coding dari volume Balok
+ * 
+ * ALGORITMA
+ * 1. Buat variable panjang
+ * 2. Buat variable lebar
+ * 3. Buat variable tinggi
+ * 4. Buat variable volume
+ * 5. Isi volume dengan rumus panjang kali lebar kali tinggi
+ * 6. Proses volume
+ * 7. Cetak volume
+ * 
+ * PSEUDOCODE
+ * MUL -> Multiply Perkalian
+ * DIV -> Divide Pembagian
+ * ADD -> Add Penjumlahan
+ * SUB -> Substract Pengurangan
+ * 
+ * BEGIN
+ *  STORE panjang WITH ANY NUMBER
+ *  STORE lebar WITH ANY NUMBER
+ *  STORE tinggi WITH ANY NUMBER
+ *  
+ *  STORE volume WITH panjang MUL lebar MUL tinggi
+ *  PRINT volume
+ * END
+ */
+// CODING
+let panjang = 10;
+let lebar = 5;
+let tinggi = 3;
+let volume = panjang * lebar * tinggi;
+console.log(volume);
+
+
+/**
+ * Study Case 2
+ * Buat algoritma, pseudocode, dan coding dari 
+ * Luas Lingkaran
+ * 
+ * ALGORITMA
+ * 1. Buat variable r
+ * 2. Buat variable phi isi dengan 3.14
+ * 3. Buat variable luas
+ * 4. Isi luas dengan rumus phi kali r kali r dan proses
+ * 5. cetak luas
+ * PSEUDOCODE
+ * BEGIN
+ *  STORE r WITH ANY NUMBER
+ *  STORE phi WITH 3.14
+ * 
+ *  STORE luas WITH phi MUL r MUL r
+ *  PRINT luas
+ *  END
+ */
+
+//CODING
+
+let r= 7;
+let phi= 3.14;
+
+let luas = phi * r * r;
+console.log(luas);
